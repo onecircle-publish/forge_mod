f31c15bfc70769f70b82b2e7faa623dbd121b21b
Circkoooooo <2389451262@qq.com>
2024-04-07 22:53:41 +0800

    自定义物品、方块

---
+++ src/main/java/com/circle/circlemod/CircleMod.java
@@ -0,0 +1,31 @@
+package com.circle.circlemod;
+
+import com.circle.circlemod.block.ModBlocks;
+import com.circle.circlemod.items.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CircleMod.MOD_ID)
+public class CircleMod {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "circlemod";
+
+
+    public CircleMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.registry(modEventBus);
+        ModBlocks.registry(modEventBus);
+
+        modEventBus.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}
---
+++ src/main/java/com/circle/circlemod/block/ModBlocks.java
@@ -0,0 +1,42 @@
+package com.circle.circlemod.block;
+
+import com.circle.circlemod.CircleMod;
+import com.circle.circlemod.items.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, CircleMod.MOD_ID);
+
+    public static RegistryObject<Block> TEST_BLOCK = registryBlock("test_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+            CreativeModeTab.TAB_MISC);
+
+    private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block,
+                                                                    CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registryBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registryBlockItem(String name, RegistryObject<T> block,
+                                                            CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    public static void registry(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+
+    }
+}
