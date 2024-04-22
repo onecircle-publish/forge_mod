b9a052b58b9a087c1d3548d83efcb5fe3ce65250
Circkoooooo <2389451262@qq.com>
2024-04-23 02:15:44 +0800

    修改僵尸的AI逻辑部分

---
+++ src/main/java/com/circle/circlemod/ai/ZombieAttachSameTypeGoal.java
@@ -0,0 +1,80 @@
+package com.circle.circlemod.ai;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.target.TargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.EnumSet;
+import java.util.function.Predicate;
+
+public class ZombieAttachSameTypeGoal<T extends LivingEntity> extends TargetGoal {
+    private Mob mob;
+    private Class<T> targetType;
+    @Nullable
+    protected LivingEntity target;
+    protected TargetingConditions targetConditions;
+
+    public ZombieAttachSameTypeGoal(Mob pMob, Class<T> pTargetType, boolean pMustSee) {
+        this(pMob, pTargetType, pMustSee, false, (Predicate<LivingEntity>) null);
+    }
+
+    public ZombieAttachSameTypeGoal(Mob pMob, Class<T> pTargetType, boolean pMustSee, boolean pMustReach) {
+        this(pMob, pTargetType, pMustSee, pMustReach, (Predicate<LivingEntity>) null);
+    }
+
+    public ZombieAttachSameTypeGoal(Mob pMob, Class<T> pTargetType, boolean pMustSee, boolean pMustReach,
+                                    @javax.annotation.Nullable Predicate<LivingEntity> pTargetPredicate) {
+        super(pMob, pMustSee, pMustReach);
+        this.targetType = pTargetType;
+        this.mob = pMob;
+        this.setFlags(EnumSet.of(Goal.Flag.TARGET));
+        this.targetConditions =
+                TargetingConditions.forCombat().range(this.getFollowDistance()).selector(pTargetPredicate);
+
+    }
+
+    public AABB getTargetSearchArea(double pTargetDistance) {
+        return this.mob.getBoundingBox().inflate(pTargetDistance, 4.0D, pTargetDistance);
+    }
+
+
+    public double getFollowDistance() {
+        return this.mob.getAttributeValue(Attributes.FOLLOW_RANGE);
+    }
+
+    @Override
+    public boolean canUse() {
+        return findTarget();
+    }
+
+    public boolean findTarget() {
+        this.target = mob.level.getNearestEntity(this.mob.level.getEntitiesOfClass(this.targetType,
+                this.getTargetSearchArea(this.getFollowDistance()), (v) -> {
+                    return true;
+                }), this.targetConditions, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
+
+        if (this.target != null) {
+            return true;
+        } else {
+            this.setFlags(EnumSet.of(Flag.LOOK));
+            return false;
+        }
+
+    }
+
+    @Override
+    protected boolean canAttack(@Nullable LivingEntity pPotentialTarget, TargetingConditions pTargetPredicate) {
+        return super.canAttack(pPotentialTarget, pTargetPredicate);
+    }
+
+    @Override
+    public void start() {
+        this.mob.setTarget(this.target);
+        super.start();
+    }
+}
--- src/main/java/com/circle/circlemod/event/ModEvents.java
+++ src/main/java/com/circle/circlemod/event/ModEvents.java
@@ -1,9 +1,22 @@
 package com.circle.circlemod.event;
 
 import com.circle.circlemod.CircleMod;
+import com.circle.circlemod.ai.ZombieAttachSameTypeGoal;
+import com.circle.circlemod.effect.ModEffects;
+import com.circle.circlemod.paticle.ModParticles;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.ZombieEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -18,5 +31,29 @@ public class ModEvents {
             player.sendMessage(Component.nullToEmpty(player.getName().getString() + " hurt a " + event.getEntity().getName().getString()),
                     UUID.randomUUID());
         }
+    }
+
+
+    @SubscribeEvent
+    public static void onLivingUpdateEvent(LivingEvent.LivingUpdateEvent e) {
+//        判断效果存在期间执行逻辑的样例，可以判断是否存在魅惑菇的魅惑效果，然后给实体添加爱心粒子
+//        LivingEntity entityLiving = e.getEntityLiving();
+//        if (entityLiving instanceof Zombie) {
+//            MobEffectInstance effect = entityLiving.getEffect(ModEffects.freezeEffect.get());
+//            if ((effect != null) && effect.getDuration() != 0) {
+//                Vec3 pos = entityLiving.position();
+//                // 产生10个粒子，粒子移动会从脚部位移动到头部
+//                for (int i = 0; i < 10; i++) {
+//                    entityLiving.level.addParticle(ModParticles.FREEZE_PARTICLES.get(), pos.x, pos.y, pos.z, 0, 10,
+//                    0);
+//                }
+//            }
+//        }
+    }
+
+    @SubscribeEvent
+    public static void onZombieEvent(ZombieEvent e) {
+        Zombie summoner = e.getSummoner();
+        summoner.targetSelector.addGoal(1, new ZombieAttachSameTypeGoal(summoner, Zombie.class, false));
     }
 }
