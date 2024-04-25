527333bb62b6aa2ca75e86e5821d2e1d84dbdea2
Circkoooooo <2389451262@qq.com>
2024-04-26 04:05:33 +0800

    设置魅惑菇模型的渲染类型为cutout

--- src/main/java/com/circle/circlemod/CircleMod.java
+++ src/main/java/com/circle/circlemod/CircleMod.java
@@ -1,30 +1,27 @@
 package com.circle.circlemod;
 
+import com.circle.circlemod.block.CircleBlocks;
 import com.circle.circlemod.block.ModBlocks;
 import com.circle.circlemod.effect.ModEffects;
 import com.circle.circlemod.item.ModItems;
 import com.circle.circlemod.paticle.ModParticles;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.JsonDeserializationContext;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.blockstates.VariantProperty;
+import net.minecraftforge.client.model.IModelLoader;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.client.model.geometry.IModelGeometry;
+import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
+import net.minecraftforge.client.model.generators.loaders.OBJLoaderBuilder;
 import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.client.model.obj.OBJModel;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.io.FileWriter;
-import java.io.IOException;
 
 @Mod(CircleMod.MOD_ID)
 public class CircleMod {
@@ -43,5 +40,6 @@ public class CircleMod {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(CircleBlocks.CHARM_BLOCK.get(), RenderType.cutout());
     }
 }
