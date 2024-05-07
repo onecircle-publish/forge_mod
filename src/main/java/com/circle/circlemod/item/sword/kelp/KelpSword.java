2075ab220fe1f0656bca4ddc179f5dfabbb5ae55
yuanxin <yuanxin@dtstack.com>
2024-05-07 15:31:37 +0800

    增加海带剑的技能函数

--- src/main/java/com/circle/circlemod/item/sword/kelp/KelpSword.java
+++ src/main/java/com/circle/circlemod/item/sword/kelp/KelpSword.java
@@ -1,5 +1,8 @@
 package com.circle.circlemod.item.sword.kelp;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 
@@ -9,4 +12,50 @@ public class KelpSword extends SwordItem {
     }
 
 
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+        return super.onLeftClickEntity(stack, player, entity);
+    }
+
+    /**
+     * 缠绕
+     */
+    public void entanglement() {
+//        判断NBT标签是否包含该属性，则在左键的时候调用
+    }
+
+    /**
+     * 水分吸收
+     */
+    public void absorption() {
+
+    }
+
+    /**
+     * 护盾
+     */
+    public void shield() {
+
+    }
+
+    /**
+     * 再生
+     */
+    public void regeneration() {
+
+    }
+
+    /**
+     * 潮汐之力
+     */
+    public void forceOfTidal() {
+
+    }
+
+    /**
+     * 水之感知
+     */
+    public void waterPerception(){
+
+    }
 }
