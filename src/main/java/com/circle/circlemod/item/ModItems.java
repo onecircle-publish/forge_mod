3293659589a096533b301118191c84d1d0a57c48
Circkoooooo <2389451262@qq.com>
2024-05-24 01:23:08 +0800

    实现冰晶法杖的贴图等

--- src/main/java/com/circle/circlemod/item/ModItems.java
+++ src/main/java/com/circle/circlemod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.circle.circlemod.item;
 
 import com.circle.circlemod.CircleMod;
 import com.circle.circlemod.item.kelp_soap.KelpSoap;
+import com.circle.circlemod.item.staff.IceStaff;
 import com.circle.circlemod.item.sword.kelp.KelpSwordItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,6 +16,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CircleMod.MOD_ID);
     public static RegistryObject<Item> KELP_SWORD = ITEMS.register("kelp_sword", () -> new KelpSwordItem(ModTiers.KELP_SWORD, 4, 2, new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
     public static RegistryObject<Item> KELP_SOAP = ITEMS.register("kelp_soap", () -> new KelpSoap(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.TEST_TAB)));
+    public static RegistryObject<Item> ICE_STAFF = ITEMS.register("ice_staff", () -> new IceStaff(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.TEST_TAB)));
 
     public static RegistryObject<Item> registerItem(String name, final Supplier sup) {
         return ITEMS.register(name, sup);
---
+++ src/main/java/com/circle/circlemod/item/staff/IceStaff.java
@@ -0,0 +1,12 @@
+package com.circle.circlemod.item.staff;
+
+import net.minecraft.world.item.Item;
+
+/**
+ * 水晶权杖
+ */
+public class IceStaff extends Item {
+    public IceStaff(Properties pProperties) {
+        super(pProperties);
+    }
+}
