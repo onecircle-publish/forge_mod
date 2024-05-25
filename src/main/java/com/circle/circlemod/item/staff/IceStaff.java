1a4b03546d37db02d0ae041b59284376a143ef22
Circkoooooo <2389451262@qq.com>
2024-05-26 01:12:42 +0800

    实现水晶法杖使用时射出箭矢，需要改成冰冻子弹

--- src/main/java/com/circle/circlemod/item/staff/IceStaff.java
+++ src/main/java/com/circle/circlemod/item/staff/IceStaff.java
@@ -1,12 +1,66 @@
 package com.circle.circlemod.item.staff;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+
+import java.util.function.Predicate;
 
 /**
  * 水晶权杖
  */
-public class IceStaff extends Item {
+public class IceStaff extends ProjectileWeaponItem {
     public IceStaff(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public Predicate<ItemStack> getAllSupportedProjectiles() {
+        return ARROW_ONLY;
+    }
+
+    @Override
+    public int getDefaultProjectileRange() {
+        return 15;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 72000;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.BOW;
+    }
+
+    @Override
+    public void onUsingTick(ItemStack stack, LivingEntity player, int count) {
+        super.onUsingTick(stack, player, count);
+    }
+
+    @Override
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity, int pTimeCharged) {
+        Player player = (Player) pLivingEntity;
+        player.stopUsingItem();
+        if (!pLevel.isClientSide) {
+            ArrowItem arrowitem = (ArrowItem) Items.ARROW;
+            AbstractArrow abstractarrow = arrowitem.createArrow(pLevel, pStack, player);
+            abstractarrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 3.0F, 1.0F);
+            abstractarrow.setCritArrow(true);
+
+            pLevel.addFreshEntity(abstractarrow);
+        }
+        super.releaseUsing(pStack, pLevel, pLivingEntity, pTimeCharged);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        pPlayer.startUsingItem(InteractionHand.MAIN_HAND);
+        return InteractionResultHolder.consume(pPlayer.getItemInHand(pUsedHand));
     }
 }
