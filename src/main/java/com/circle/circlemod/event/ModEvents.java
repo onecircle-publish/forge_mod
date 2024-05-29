16b62326744a71471f9700d1c690472d863ffa17
Circkoooooo <2389451262@qq.com>
2024-05-30 05:33:59 +0800

    实现丢弃的逻辑

--- src/main/java/com/circle/circlemod/event/ModEvents.java
+++ src/main/java/com/circle/circlemod/event/ModEvents.java
@@ -1,13 +1,28 @@
 package com.circle.circlemod.event;
 
 import com.circle.circlemod.CircleMod;
+import com.circle.circlemod.item.staff.IceStaff;
 import com.circle.circlemod.item.sword.kelp.KelpSwordItem;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = CircleMod.MOD_ID)
 public class ModEvents {
+    @SubscribeEvent
+    public static void playerInteractive(PlayerInteractEvent.EntityInteractSpecific event) {
+        InteractionHand usedItemHand = event.getPlayer().getUsedItemHand();
+        Item itemInHand = event.getPlayer().getItemInHand(usedItemHand).getItem();
+        Entity target = event.getTarget();
+
+        // 交互时，使用的是权杖
+        if (itemInHand instanceof IceStaff) {
+            ((IceStaff) itemInHand).setInteractiveTarget(target);
+        }
+    }
 }
--- src/main/java/com/circle/circlemod/item/staff/IceStaff.java
+++ src/main/java/com/circle/circlemod/item/staff/IceStaff.java
@@ -1,20 +1,30 @@
 package com.circle.circlemod.item.staff;
 
+import com.circle.circlemod.CircleMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.RangedBowAttackGoal;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Predicate;
 
 /**
  * 水晶权杖
  */
 public class IceStaff extends ProjectileWeaponItem {
+    private Entity interactiveTarget = null;
+
     public IceStaff(Properties pProperties) {
         super(pProperties);
     }
@@ -31,7 +41,7 @@ public class IceStaff extends ProjectileWeaponItem {
 
     @Override
     public int getUseDuration(ItemStack pStack) {
-        return 72000;
+        return 20;
     }
 
     @Override
@@ -40,8 +50,10 @@ public class IceStaff extends ProjectileWeaponItem {
     }
 
     @Override
-    public void onUsingTick(ItemStack stack, LivingEntity player, int count) {
-        super.onUsingTick(stack, player, count);
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        doMagic();
+        this.interactiveTarget = null;
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
     }
 
     @Override
@@ -50,11 +62,73 @@ public class IceStaff extends ProjectileWeaponItem {
         return InteractionResultHolder.consume(pPlayer.getItemInHand(pUsedHand));
     }
 
-    public LivingEntity getEntityPlayerFacing() {
-        Entity entity = Minecraft.getInstance().crosshairPickEntity;
-        if (entity instanceof LivingEntity) {
-            return (LivingEntity) entity;
+    @Override
+    public boolean useOnRelease(ItemStack pStack) {
+
+        return super.useOnRelease(pStack);
+    }
+
+    /**
+     * 获取准心所指实体
+     *
+     * @return
+     */
+    public Entity getEntityCrosshairPick() {
+        return Minecraft.getInstance().crosshairPickEntity;
+    }
+
+    public void doMagic() {
+        Entity entityCrosshairPick = getEntityCrosshairPick();
+
+        if (entityCrosshairPick instanceof LivingEntity) {
+            magicToLivingEntity((LivingEntity) entityCrosshairPick);
         }
-        return null;
+    }
+
+    public void magicToLivingEntity(LivingEntity entity) {
+        List<ItemStack> allItems = new ArrayList<>();
+
+        // 加入丢弃列表
+        Iterable<ItemStack> allSlots = entity.getAllSlots();
+        allSlots.forEach(slot -> {
+            ItemStack item = slot.getItem().getDefaultInstance();
+            if (!item.isEmpty()) {
+                allItems.add(item);
+            }
+        });
+        // 随机丢弃一个物品
+        if (!allItems.isEmpty()) {
+            ItemStack randomItem = allItems.get(new Random().nextInt(allItems.size()));
+
+            if (this.interactiveTarget != null) {
+                ItemEntity itemEntity = interactiveTarget.spawnAtLocation(randomItem, 0);
+                Vec3 lookAngle = entity.getLookAngle();
+                itemEntity.setDeltaMovement(new Vec3(lookAngle.x, lookAngle.y, lookAngle.z));
+
+//                if (entity instanceof Mob) {
+//                    ((Mob) entity).goalSelector.getAvailableGoals().forEach(goal -> {
+//                        Goal currentGoal = goal.getGoal();
+//                        if (currentGoal instanceof RangedBowAttackGoal<?>) {
+//                            ((Mob) entity).goalSelector.removeGoal(goal);
+//                        }
+//                    });
+//                }
+
+                SlotAccess mainHand = entity.getSlot(EquipmentSlot.MAINHAND.getIndex() + LivingEntity.EQUIPMENT_SLOT_OFFSET);
+                SlotAccess offHand = entity.getSlot(EquipmentSlot.OFFHAND.getIndex() + LivingEntity.EQUIPMENT_SLOT_OFFSET);
+                if (mainHand.get().is(randomItem.getItem())) {
+                    entity.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+                    entity.swing(InteractionHand.MAIN_HAND);
+                }
+                if (offHand.get().is(randomItem.getItem())) {
+                    entity.setItemSlot(EquipmentSlot.OFFHAND, ItemStack.EMPTY);
+                    entity.swing(InteractionHand.OFF_HAND);
+                }
+            }
+        }
+    }
+
+    public void setInteractiveTarget(Entity entity) {
+        interactiveTarget = entity;
     }
 }
