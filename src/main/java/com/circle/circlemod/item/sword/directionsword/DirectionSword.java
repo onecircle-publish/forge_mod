23f0ef4a374a9875747a6379bca2fb4f3564e376
Circkoooooo <2389451262@qq.com>
2024-06-04 00:26:54 +0800

    增加粒子

--- src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
+++ src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
@@ -1,5 +1,6 @@
 package com.circle.circlemod.item.sword.directionsword;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -11,7 +12,6 @@ import net.minecraft.world.item.Tier;
  * @date : 2024-06-03 17:02
  **/
 public class DirectionSword extends SwordItem {
-
     public DirectionSword(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
     }
@@ -20,6 +20,8 @@ public class DirectionSword extends SwordItem {
      * 攻击后方
      */
     public void useDirectionHurt(ItemStack useItem, Player attacker, LivingEntity target) {
+        float yRot = attacker.getYRot();
+        attacker.level.addParticle(ParticleTypes.SWEEP_ATTACK, attacker.getX(), attacker.getY(), attacker.getZ(), 0.0D, 0.0D, 0.0D);
         this.hurtEnemy(useItem, target, attacker);
     }
 }
