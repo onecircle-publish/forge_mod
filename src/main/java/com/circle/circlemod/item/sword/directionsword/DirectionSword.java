f650893a8f3a1b26636df4183b43ef9a42a06189
yuanxin <yuanxin@dtstack.com>
2024-06-04 15:08:09 +0800

    实现攻击周围实体

--- src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
+++ src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
@@ -1,11 +1,15 @@
 package com.circle.circlemod.item.sword.directionsword;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.phys.AABB;
+
+import java.util.List;
 
 /**
  * @author : yuanxin
@@ -20,8 +24,19 @@ public class DirectionSword extends SwordItem {
      * 攻击后方
      */
     public void useDirectionHurt(ItemStack useItem, Player attacker, LivingEntity target) {
-        float yRot = attacker.getYRot();
-        attacker.level.addParticle(ParticleTypes.SWEEP_ATTACK, attacker.getX(), attacker.getY(), attacker.getZ(), 0.0D, 0.0D, 0.0D);
-        this.hurtEnemy(useItem, target, attacker);
+        if (!attacker.level.isClientSide) {
+            double attackRange = attacker.getAttackRange();
+            AABB aabb = new AABB(new BlockPos(attacker.getBlockX(), attacker.getBlockY(), attacker.getBlockZ()));
+            List<LivingEntity> livingEntities = attacker.level.getEntitiesOfClass(LivingEntity.class, aabb);
+            livingEntities.forEach(livingEntity -> {
+                double v = livingEntity.getPosition(1).distanceTo(attacker.getPosition(1));
+                if (v < attackRange) {
+                    useItem.hurtEnemy(livingEntity, attacker);
+                }
+            });
+
+            attacker.level.addParticle(ParticleTypes.SWEEP_ATTACK, attacker.getX(), attacker.getY(), attacker.getZ(), 0.0D, 0.0D, 0.0D);
+            this.hurtEnemy(useItem, target, attacker);
+        } ;
     }
 }
