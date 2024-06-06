ed6117205197e5bd7a879375ae84d36359f5818d
Circkoooooo <2389451262@qq.com>
2024-06-07 00:57:21 +0800

    实现玩家视角和实体在mc坐标系中夹角的逻辑

--- src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
+++ src/main/java/com/circle/circlemod/item/sword/directionsword/DirectionSword.java
@@ -3,7 +3,6 @@ package com.circle.circlemod.item.sword.directionsword;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -12,7 +11,7 @@ import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
 
 import java.util.List;
 
@@ -38,24 +37,28 @@ public class DirectionSword extends SwordItem {
             double attackRange = attacker.getAttackRange();
             List<Entity> entities = level.getEntities(attacker, new AABB(attacker.getOnPos()).inflate(attackRange));
 
-            float yRot = attacker.getYRot() % 360;
-            Vec3 playerPos = attacker.getPosition(1);
-
+            // 计算玩家朝向和实体之间的夹角
+            // yRot为2PI - 视角角度（0-2PI），这里需要游戏中视角向左的时候为逆时针，实际上靠左的坐标才是正坐标，所以用2PI去减
+            // vecAttacker 是玩家 到 玩家朝向的一个点 的向量，也就是玩家指向 玩家坐标向玩家朝向延长一个单位的地方 的向量。
+            float yRot = (float) (Math.toDegrees(Math.PI * 2) - (float) (Math.toDegrees(Math.PI * 2) + attacker.getYRot() % 360));
+            Vec2 vecAttacker = new Vec2(((float) Math.sin(Math.toRadians(yRot))), (float) Math.cos(Math.toRadians(yRot)));
             entities.forEach(entity -> {
-                // todo:计算正确的角度逻辑
 
+                // 这里求出玩家指向实体的向量
+                Vec2 vecTargetToAttacker = new Vec2((float) (entity.getX() - attacker.getX()), (float) (entity.getZ() - attacker.getZ()));
 
-//                if (!entity.is(attacker) && entity instanceof LivingEntity) {
-//                    double rot = Math.abs(yRot + Math.toDegrees((entity.getX() - playerPos.x) / entity.getPosition(1).distanceTo(playerPos)));
-//                    if (rot <= 225 && rot >= 135) {
-//                        attacker.attack(entity);
-//                    }
-//
-//                    double d0 = (double) (-Mth.sin(attacker.getYRot() * ((float) Math.PI / 180F)));
-//                    double d1 = (double) Mth.cos(attacker.getYRot() * ((float) Math.PI / 180F));
-//
-//                    ((ServerLevel) level).sendParticles(ParticleTypes.SWEEP_ATTACK, attacker.getX() + d0, attacker.getY(0.5D), attacker.getZ() + d1, 0, d0, 0.0D, d1, 0.0D);
-//                }
+                // vecAttacker 和 vecTargetToAttacker的夹角。点积除以长度之和的反余弦，然后转角度
+                double angle = Math.toDegrees(Math.acos(vecAttacker.dot(vecTargetToAttacker) / (vecAttacker.length() * vecTargetToAttacker.length())));
+
+                if (!(entity instanceof Player) && entity instanceof LivingEntity) {
+                    if (angle <= 225 && angle >= 135) {
+                        attacker.attack(entity);
+                    }
+
+                    double d0 = (double) (-Mth.sin(attacker.getYRot() * ((float) Math.PI / 180F)));
+                    double d1 = (double) Mth.cos(attacker.getYRot() * ((float) Math.PI / 180F));
+                    ((ServerLevel) level).sendParticles(ParticleTypes.SWEEP_ATTACK, attacker.getX() + d0, attacker.getY(0.5D), attacker.getZ() + d1, 0, d0, 0.0D, d1, 0.0D);
+                }
             });
         } else {
             level.addParticle(ParticleTypes.SWEEP_ATTACK, attacker.getX(), attacker.getEyeY(), attacker.getZ(), 0, 0, 0);
